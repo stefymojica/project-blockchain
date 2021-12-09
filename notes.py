d4fff1f2f4cb8c7b346874577e4b0c1a1779fe39
Stefy Mojica <stefymojica40@gmail.com>
2021-12-08 20:53:36 -0500

    Trabajando con Inputs

--- notes.py
+++ notes.py
@@ -35,3 +35,24 @@ add_value(0.9)
 add_value(10.36)
 
 #print(blockchain)
+
+## Working with Inputs
+
+blockchain = []
+
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction,transaction_amount])
+
+txt_amount = float(input('Your transaction amount please: '))
+add_value(txt_amount)
+
+txt_amount = float(input('Your transaction amount please: '))
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=txt_amount)
+
+txt_amount = float(input('Your transaction amount please: '))
+add_value(txt_amount, get_last_blockchain_value())
+
+# print(blockchain)
