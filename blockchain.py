70d37a8d837dbcbb941a355cc1f278caf572b9a0
Stefy Mojica <stefymojica40@gmail.com>
2021-12-14 17:01:02 -0500

    Adding if-else

--- blockchain.py
+++ blockchain.py
@@ -9,21 +9,32 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     user_input = float(input('Your transaction amount please: '))
     return user_input
 
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
-txt_amount = get_user_input()
-add_value(txt_amount)
-
-while True:
-    txt_amount = get_user_input()
-    add_value(txt_amount, get_last_blockchain_value())
-
+def print_blockchain_elements():
     # Output the blockchain list to the console
     for block in blockchain:
         print('Outputting Block')
         print(block)
 
+txt_amount = get_transaction_value()
+add_value(txt_amount)
+
+while True:
+    print('Please choose: ')
+    print('1: Add a new transaction value')
+    print('2: Outout the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        txt_amount = get_transaction_value()
+        add_value(txt_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
+        
 print('Done!')
